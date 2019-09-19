a7512da77e4dab6e0a3198cdfa45b26534caaa58
mesagoh <mesagoh@gmail.com>
2019-09-19 11:47:02 -0700

    renders list of unicorns. TODO: change li to flexbox maybe

--- src/components/HorseSelection.vue
+++ src/components/HorseSelection.vue
@@ -1,18 +1,44 @@
 <template>
     <div class="horseBox">
-        <img src="../assets/unicorn.png" alt="Unicorn" height="300" width="300">
+        <ul id="horseMenu">
+            <li v-bind:key="horse.key" v-for="horse of horseItems" v-on:click="showModal">
+                <img :src="require(`@/assets/${horse.imgSrc}`)" alt="Unicorn" height="300" width="300">
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'HorseSelection'
+  name: 'HorseSelection',
+  data () {
+    return {
+      horseItems: [
+        {key: 1, imgSrc: 'unicorn.png'},
+        {key: 2, imgSrc: 'unicorn.png'},
+        {key: 3, imgSrc: 'unicorn.png'}
+      ]
+    }
+  },
+  methods: {
+    showModal () {
+      console.log('clicked!')
+    }
+  }
 }
 </script>
 
 <style scoped>
 .horseBox {
     background: grey;
+}
+
+ul#horseMenu li{
+    display: inline;
+}
+img {
+    margin: 10px 10px;
+    background: white;
 }
 
 </style>
